407acb3e9cde14b27565f14f8aaa9b90ba962076
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2024-09-15 15:48:07 +0300

    added nav bar and hero section

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',debug=True)
