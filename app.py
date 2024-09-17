ce6cc9265341247296ca8bf7574807af17b6f095
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2024-09-17 12:49:56 +0300

    added the Job table and Job listing card

--- app.py
+++ app.py
@@ -16,7 +16,20 @@ class User(db.Model):
     username = db.Column(db.String(255), unique=True) 
     email = db.Column(db.String(30), primary_key=True, nullable=False)
     password = db.Column(db.String(10), nullable=False)
-    
+
+
+class JobPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+    job_type = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    salary = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    company = db.Column(db.String(255), nullable=False)
+    company_logo = db.Column(db.String(255), nullable=True)
+    company_website = db.Column(db.String(255), nullable=False)
+
     
 
 @app.route('/')
