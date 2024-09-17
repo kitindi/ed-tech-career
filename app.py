6cfc5cfc68a6012e5893291ee15f79b0b201d80f
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2024-09-17 08:16:29 +0300

    ctreated user model and db config

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, redirect, url_for
 
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
+# create a user model
+
+class User(db.Model):
+    username = db.Column(db.String(255), unique=True) 
+    email = db.Column(db.String(30), primary_key=True, nullable=False)
+    password = db.Column(db.String(10), nullable=False)
+    
+    
 
 @app.route('/')
 def index():
@@ -15,4 +31,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='127.0.0.1',debug=True)
